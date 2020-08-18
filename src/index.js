94646739b75dfd50e3ec7e4f85877639ba69cfb4
Xavier Ritch <xritch89@hotmail.com>
2020-08-18 12:16:05 -0400

    pears

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import $ from 'jquery';
 import 'normalize.css';
 import './index.css';
 import api from './api';
-
+import store from './store'
 import shoppingList from './shopping-list';
 
 
 const main = function () {
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
   api.createItem('pears')
   .then(res => res.json())
   .then((newItem) => {
