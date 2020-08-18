62c84c7b1d00430f29f1f624f24bb2fd234d9c8c
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-18 12:09:37 -0400

    step 4

--- src/api.js
+++ src/api.js
@@ -1,9 +1,16 @@
 export default {
-  getItems
+  getItems,
+  createItem
 };
 
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/xavier-jason';
 
 function getItems() {
     return fetch(`${BASE_URL}/items`);
+}
+
+function createItem(name) {
+  let object = {name: name};
+  let newItem = JSON.stringify(object);
+  return fetch(`${BASE_URL}/items`, {method:'POST',headers:{'Content-Type': 'application/json'}, body: newItem});
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import shoppingList from './shopping-list';
 
 
 const main = function () {
+  api.createItem('pears')
+  .then(res => res.json())
+  .then((newItem) => {
+    return api.getItems();
+  })
+  .then(res => res.json())
+  .then((items) => {
+    console.log(items);
+  });
+  
   api.getItems()
     .then(res => console.log(res));
 
