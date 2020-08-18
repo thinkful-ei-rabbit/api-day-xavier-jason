a529405f289917dbeb6093fd5bdd9c67150d7888
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-18 11:36:27 -0400

    step 2

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export default {
+  getItems
+};
+
+const BASE_URL = 'https://thinkful-list.api.herokuapp.com/xavier-jason';
+
+function getItems() {
+  return Promise.resolve('A successful response!');
+}
